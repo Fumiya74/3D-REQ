88223c8c921aa9f008b145afea8173117f9115e8
松澤郁哉 <matsuzawafumiya@matsuzawakoujinoMacBook-Air.local>
2022-08-02 16:25:25 +0900

    edit re_gem

--- ssg2req/scripts/re_gem.py
+++ ssg2req/scripts/re_gem.py
@@ -1,5 +1,8 @@
-def Ref_Generator(*ref_exp,**objects):
-    for object in objects:
-        target_id = object['id']
-        scene_id = object['scene_id']
-        
+import random
+#再帰の時の
+#不確定性を計算する関数
+def Ref_Generator(tar_n,*ref_exp,**objects):
+ 
+
+        ref_exp_set = set(ref_exp)
+        start = False
