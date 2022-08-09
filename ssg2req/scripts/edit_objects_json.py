340e2261e8e251c7751e954d7578d5b168dfa5b9
fumiya74 <f.matsuzawa719@gmail.com>
2022-08-09 14:23:13 +0900

    add spatial relationships

--- ssg2req/scripts/edit_objects_json.py
+++ ssg2req/scripts/edit_objects_json.py
@@ -1,10 +1,10 @@
 #最終的に作りたいフォーマットは全質問がリストになっていて、１つの質問ごとに辞書型で詳細を記述
 import json
 
-file_path = "../../data/edited_objects.json"
+file_path = "../../data/edited_objects_v2.json"
 objects_json = open("../../data/objects.json","r")
 objects = json.load(objects_json)
-r_open_file_path = '../../data/edited_relationshios.json'
+r_open_file_path = '../../data/edited_relationships_v2.json'
 r_file_open = open(r_open_file_path,'r')
 r_file = json.load(r_file_open)
 
@@ -18,6 +18,7 @@ all_objects = []
 #print(isinstance(objects["scans"][0],dict))
 #True
 scenes = objects["scans"]
+
 for scene in scenes:
     #現在探索中のscan名を変数で記憶
     scene_id = scene["scan"] 
@@ -31,16 +32,26 @@ for scene in scenes:
         object.pop('eigen13')
         object.pop('rio27')
         object.pop('affordances',None)
-        object['relationships'] = []
+        obj_rel = []
+
         for relationships in r_file:
             if relationships['scene_id'] == scene_id:
                 for relationship in relationships['relationships']:
+                    #print([str(relationship[0])])
                     if str(relationship[0]) == object['id']:
-                        object['relationships'].append([str(relationship[1]),relationship[3]])
-                #一回該当箇所が出たらもう登場しないからブレイク
-                break
-
+                        
+                        #no_anchor_class以外＆ターゲットと別のクラス
+                        if relationship[4] not in relationships['no_anchor_class']:
+                            #print([str(relationship[1]),relationship[3]])
+                            obj_rel.append([relationship[3],relationship[4]])
+                
+        #print(obj_rel)
+        #2次元リストはセットにわたせなかったから2次元目の要素をタプルに一回変更
+        obj_rel = [tuple(i) for i in obj_rel]
+        obj_rel = list(set(obj_rel))
+        obj_rel = [list(i) for i in obj_rel]
+        object['relationships'] = obj_rel
         all_objects.append(object)
-#TODO same_class_listの作成
+
 with open(file_path,'w') as outfile:
     json.dump(all_objects, outfile, indent=2)
