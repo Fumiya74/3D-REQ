21e252df6674aa6ac0c3d5cfb5efa9bbe96d3e68
松澤郁哉 <matsuzawafumiya@matsuzawakoujinoMacBook-Air.local>
2022-08-12 17:04:45 +0900

    add list

--- ssg2req/scripts/sentence_generator.py
+++ ssg2req/scripts/sentence_generator.py
@@ -14,26 +14,28 @@ def main():
     specify_words = ["choose","select","look"]
 
     for f in file:
-        color = [""]
-        shape = []
-        size = []
-        material = []
-        texture = []
+        color = ["white","black","green","blue","red","brown","yellow","gray","orange","purple","pink","beige","bright","dark","light","silver","gold"]
+        shape = ["round","flat","L-shaped","semicircular","circular","square","rectangular","sloping","cylindrical","oval","bunk","heart-shaped","u-shaped","octagon"]
+        size = ["big","tall","low","narrow","wide"]
+        material = ["wooden","plastic","metal","glass","stone","leather","concrete","ceramic","brick","padded","cardboard","marbled","carpet","cork","velvet"]
+        texture = ["striped","patterned","dotted","colorful","checker","painted","shiny",""]
 
         for refer in f["refer"][1:]:
             if refer in color_list:
                 color.append(refer)
-            if refer in shape_list:
+            elif refer in shape_list:
                 shape.append(refer)
-            if refer in size_list:
+            elif refer in size_list:
                 size.append(refer)
-            if refer in material_list:
+            elif refer in material_list:
                 material.append(refer)
-            if refer in texture_list:
+            elif refer in texture_list:
                 texture.append(refer)
-            if isinstance(refer,list):
+            elif isinstance(refer,list):
                 if len(refer) == 2:
                     relationship = refer
                 if len(refer) == 1:
                     comparative = refer
+            else:
+                print("リストに何かしら不足あり")
             
