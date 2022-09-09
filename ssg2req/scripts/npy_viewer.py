5e7e1ee0cf077cd417907161696df4d3ea550645
fumiya74 <f.matsuzawa719@gmail.com>
2022-09-09 18:01:29 +0900

    debug format.py

--- ssg2req/scripts/npy_viewer.py
+++ ssg2req/scripts/npy_viewer.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 #print(cap["arr_0"])
 #cap = np.load('../../data/datasets/0988ea72-eb32-2e61-8344-99e2283c2728_0_question.npz')
 #print(cap["arr_0"])
-cap = np.load('test_pc.npz')
-for c in cap['pc']:
+cap = np.load('/home/fumiya/matsu/CyREx/data/datasets/train/001443_bbox.npy')
+#for c in cap["arr_0"]:
+#    print(c)
+#print(cap["arr_0"])
+for c in cap:
     print(c)
